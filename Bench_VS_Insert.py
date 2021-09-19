99aa622d999c1366ed080e12dafe186a6dc3eb07
Vadim Pozdnyakov <vadim.pozdnyakov1998@gmail.com>
2021-09-20 00:32:34 +0300

    Cosmetic

--- Bench_VS_Insert.py
+++ Bench_VS_Insert.py
@@ -17,7 +17,7 @@ class Bench_Valentina_Record_CRUD( BenchServer_Valentina ):
     def SetUp(self):
         self.AddArtefact('Record Count', self.ScalableValue)
 
-        self.cursor.execute('DROP TABLE IF EXISTS T1')
+        self.cursor.execute(self.cmd_DropTable)
         self.cursor.execute(self.cmd_CreateTable)
 
     def TearDown(self):
@@ -26,13 +26,12 @@ class Bench_Valentina_Record_CRUD( BenchServer_Valentina ):
     def InsertRecords(self):
         for i in self.Range(0, self.ScalableValue):
             self.cursor.execute( self.cmd_Insert, [i, i] )
-            # self.cursor.execute( 'INSERT INTO T1 VALUES (' + str(i) + ');' )
 
     def UpdateRecords(self):
         for i in self.Range(0, self.ScalableValue):
             self.cursor.execute( self.cmd_Update, [i+i, i] )
 
-    def DropRecords(self):
+    def DeleteRecords(self):
         for i in self.Range(0, self.ScalableValue):
             self.cursor.execute( self.cmd_Delete, [i] )
     
@@ -42,12 +41,13 @@ class Bench_Valentina_Record_CRUD( BenchServer_Valentina ):
 
     def BenchBody(self):
         self.TimeLog( 'Insert', self.InsertRecords )
-        self.TimeLog( 'Select', self.SelectRecords )
         self.TimeLog( 'Update', self.UpdateRecords )
-        self.TimeLog( 'Drop', self.DropRecords )
+        self.TimeLog( 'Select', self.SelectRecords )
+        self.TimeLog( 'Delete', self.DeleteRecords )
 
 Bench_Valentina_Record_CRUD.cmd_CreateTable = 'CREATE TABLE T1( fld_val LONG, fld_key LONG INDEXED);'
-Bench_Valentina_Record_CRUD.cmd_DropTable = 'DROP TABLE T1;'
+Bench_Valentina_Record_CRUD.cmd_DropTable = 'DROP TABLE IF EXISTS T1;'
+
 Bench_Valentina_Record_CRUD.cmd_Insert = 'INSERT INTO T1 VALUES (:1, :2);'
 Bench_Valentina_Record_CRUD.cmd_Update = 'UPDATE T1 SET fld_val = :1 WHERE fld_key = :2 ;'
 Bench_Valentina_Record_CRUD.cmd_Delete = 'DELETE FROM T1 WHERE fld_key = :1 ;'
@@ -59,10 +59,11 @@ def main():
     ##########
     # Valentina bench
     VS_Server_Addr = 'sa:sa@127.0.0.1/vdb_test_db'
-    # VS_Server_Axe = [10, 100, 1000, 10000, 100000]
+    # VS_Server_Addr = '/home/lord/bench/Bench_VS_Insert.vdb'
+    # VS_Server_Axe = [1000, 10000, 100000]
     VS_Server_Axe = [100000]
     VS_Bench = Bench_Valentina_Record_CRUD(VS_Server_Addr)
-    # VS_Bench.put_BenchName('VS_Record_CRUD') # optional
+    VS_Bench.put_BenchName('VS_Record_CRUD') # optional
     VS_Bench.put_Scalable( VS_Server_Axe )
 
     # Forward
